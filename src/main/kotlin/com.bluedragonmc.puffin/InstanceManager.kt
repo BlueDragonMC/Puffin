471ba671f29547d4c7cb82e970370e8e9e38f325
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-07-12 13:33:03 -0400

    Add more logging and make game type matching more forgiving for nullable values

--- src/main/kotlin/com.bluedragonmc.puffin/InstanceManager.kt
+++ src/main/kotlin/com.bluedragonmc.puffin/InstanceManager.kt
@@ -4,9 +4,13 @@ import com.bluedragonmc.messages.GameType
 import com.bluedragonmc.messages.NotifyInstanceCreatedMessage
 import com.bluedragonmc.messages.NotifyInstanceRemovedMessage
 import com.bluedragonmc.messagingsystem.AMQPClient
+import org.slf4j.LoggerFactory
 import java.util.*
 
 object InstanceManager {
+
+    private val logger = LoggerFactory.getLogger(InstanceManager::class.java)
+
     /**
      * A map of instance IDs to their running game types
      */
@@ -23,7 +27,7 @@ object InstanceManager {
             instanceTypes[message.instanceId] = message.gameType
 
             // Add to list of containers
-            if(!containers.containsKey(message.containerId)) {
+            if (!containers.containsKey(message.containerId)) {
                 containers[message.containerId] = mutableSetOf()
             }
             containers[message.containerId]!!.add(message.instanceId)
@@ -33,20 +37,27 @@ object InstanceManager {
             // Check if this was the instance requested to start by the Queue system
             val m = message.gameType
             val q = Queue.startingInstance ?: return@subscribe
-            if(m.name == q.name && (q.mode == null || m.mode == q.mode) && (q.mapName == null || m.mapName == q.mapName)) {
+            if (m.name == q.name && (q.mode == null || m.mode == q.mode) && (q.mapName == null || m.mapName == q.mapName)) {
+                logger.info("The instance requested by the Queue system has started: gameType=$m, instanceId=${message.instanceId}, containerId=${message.containerId}")
                 Queue.startingInstance = null
             }
         }
         client.subscribe(NotifyInstanceRemovedMessage::class) { message ->
             containers[message.containerId]!!.remove(message.instanceId)
+            instanceTypes.remove(message.instanceId)
         }
     }
 
     fun getGameType(instanceId: UUID) = instanceTypes[instanceId]
-    fun findInstancesOfType(gameType: GameType, matchMapName: Boolean = false, matchGameMode: Boolean = true): Map<UUID, GameType> {
+    fun findInstancesOfType(
+        gameType: GameType, matchMapName: Boolean = false, matchGameMode: Boolean = false
+    ): Map<UUID, GameType> {
         return instanceTypes.filter { (_, type) ->
-            type.name == gameType.name && (!matchMapName || type.mapName == gameType.mapName) && (!matchGameMode || type.mode == gameType.mode)
+            type.name == gameType.name &&
+                    (!matchMapName || (gameType.mapName == null || type.mapName == gameType.mapName)) &&
+                    (!matchGameMode || (gameType.mode == null || type.mode == gameType.mode))
         }
     }
+
     fun findContainerWithLeastInstances() = containers.minByOrNull { (_, instances) -> instances.size }
 }
--- src/main/kotlin/com.bluedragonmc.puffin/Queue.kt
+++ src/main/kotlin/com.bluedragonmc.puffin/Queue.kt
@@ -2,10 +2,12 @@ package com.bluedragonmc.puffin
 
 import com.bluedragonmc.messages.*
 import com.bluedragonmc.messagingsystem.AMQPClient
+import org.slf4j.LoggerFactory
 import java.util.UUID
 
 object Queue {
 
+    private val logger = LoggerFactory.getLogger(Queue::class.java)
     private val queue = mutableMapOf<UUID, GameType>()
     private lateinit var client: AMQPClient
     internal var startingInstance: GameType? = null
@@ -13,12 +15,14 @@ object Queue {
     fun start(client: AMQPClient) {
         this.client = client
         client.subscribe(RequestAddToQueueMessage::class) { message ->
+            logger.info("${message.player} added to queue for ${message.gameType}")
             queue[message.player] = message.gameType
             client.publish(SendChatMessage(message.player, "<green>You are now queued for ${message.gameType.name}."))
             update()
         }
 
         client.subscribe(RequestRemoveFromQueueMessage::class) { message ->
+            logger.info("${message.player} removed from queue")
             queue.remove(message.player)
             client.publish(SendChatMessage(message.player, "<red>You have been removed from the queue."))
         }
@@ -26,11 +30,21 @@ object Queue {
 
     fun update() {
         queue.entries.removeAll { (player, gameType) ->
-            val instances = InstanceManager.findInstancesOfType(gameType, matchGameMode = true)
+            val instances = InstanceManager.findInstancesOfType(gameType, matchMapName = false, matchGameMode = false)
+            logger.info("Found ${instances.size} instances matching the $player's requested game type: $gameType")
             if(instances.isNotEmpty()) {
-                val (best, _) = instances.minBy { GameStateManager.getEmptySlots(it.key) }
+
+                val (best, _) = instances.keys.associateWith {
+                    GameStateManager.getEmptySlots(it)
+                }.filter { it.value > 0 }.entries.minByOrNull { it.value } ?: run {
+                    logger.info("All instances of type $gameType have no empty player slots.")
+                    return@removeAll false
+                }
+
+                logger.info("Instance with least empty slots for $gameType: $best")
                 if(GameStateManager.getEmptySlots(best) > 0) {
                     // Send the player to this instance and remove them from the queue
+                    logger.info("Found instance for player $player: instanceId=$best, gameType=$gameType")
                     client.publish(SendPlayerToInstanceMessage(player, best))
                     return@removeAll true
                 }
@@ -39,9 +53,11 @@ object Queue {
         }
         if(startingInstance != null) return
         queue.entries.firstOrNull()?.let { (player, gameType) ->
+            logger.info("Starting a new instance for player $player because they could not find any instances running $gameType.")
             // Create a new instance for the first player in the queue.
             startingInstance = gameType
-            val container = InstanceManager.findContainerWithLeastInstances()?.key ?: return@let
+            val (container, instances) = InstanceManager.findContainerWithLeastInstances() ?: return@let
+            logger.info("The container with the least instances is $container with ${instances.size} instances running.")
             client.publish(RequestCreateInstanceMessage(container, gameType))
             client.publish(SendChatMessage(player, "<aqua>Creating a new instance..."))
         }
