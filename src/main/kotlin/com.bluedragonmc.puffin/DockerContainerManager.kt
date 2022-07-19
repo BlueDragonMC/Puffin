6a5c87fdccae65d0d121190a5e94f91dc09c0295
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-07-18 21:48:59 -0400

    Initial work on Docker orchestration and automatic image building/updating

---
+++ src/main/kotlin/com.bluedragonmc.puffin/DockerContainerManager.kt
@@ -0,0 +1,325 @@
+package com.bluedragonmc.puffin
+
+import com.bluedragonmc.messagingsystem.AMQPClient
+import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.api.model.Container
+import com.github.dockerjava.api.model.HostConfig
+import com.github.dockerjava.core.DefaultDockerClientConfig
+import com.github.dockerjava.core.DockerClientImpl
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.apache.commons.compress.archivers.tar.TarFile
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
+import org.apache.commons.io.IOUtils
+import org.slf4j.LoggerFactory
+import java.io.*
+import java.net.URI
+import java.net.URL
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+
+object DockerContainerManager {
+
+    private val logger = LoggerFactory.getLogger(DockerContainerManager::class.java)
+
+    private lateinit var docker: DockerClient
+    private val http = HttpClient.newHttpClient()
+
+    private const val GITHUB_USER = "BlueDragonMC"
+
+    private const val PUFFIN_NETWORK_NAME = "puffin_network"
+
+    /**
+     * A list of Docker image names whose containers must be connected to the Puffin network.
+     */
+    private val requiredNetworkAccess = listOf("mongo", "rabbitmq", "puffin")
+
+    /**
+     * A list of repositories whose versions are checked on startup if they aren't saved.
+     */
+    private val repositoriesToVersionCheck = listOf("Server", "Komodo").associateWith { "main" }
+
+    private lateinit var puffinNetworkId: String
+
+    /**
+     * The number of containers to maintain per repository.
+     * If there are not enough containers of this type, more will
+     * be created every 30 seconds until the threshold is reached.
+     */
+    private var numContainersByLabel = listOf(
+        DockerImageType("Server", 2),
+        DockerImageType("Komodo", 1)
+    )
+
+    data class DockerImageType(val label: String, val minimumRequiredContainers: Int, val imageName: () -> String) {
+        constructor(repo: String, minimum: Int) : this(getVersionLabel(repo), minimum, { getMostRecentTag(repo) })
+    }
+
+    /**
+     * Get the Docker image tag for a specific repository and version
+     */
+    private fun getTag(repo: String, version: String) = "$GITHUB_USER/$repo:$version".lowercase()
+
+    /**
+     * Get the Docker image tag for a specific repository using the latest version
+     */
+    private fun getMostRecentTag(repo: String) = getTag(repo, getMostRecentVersion(repo) ?: "latest")
+
+    /**
+     * Get the latest version of a repository from the properties file.
+     * Note that this can return a null value. If this happens, the version should be fetched from GitHub.
+     */
+    private fun getMostRecentVersion(repo: String): String? {
+        return SavedProperties.getString("latest_version_$repo")
+    }
+    private fun setMostRecentVersion(repo: String, commit: String) {
+        SavedProperties.setString("latest_version_$repo", commit)
+        SavedProperties.save()
+        logger.info("Latest version of $repo was updated to: $commit (Properties file saved)")
+    }
+
+    /**
+     * Get the label that is added to a Docker image when it is built to specify the version of the repository it is running.
+     */
+    private fun getVersionLabel(repository: String) = "com.bluedragonmc.${repository.lowercase()}.version"
+
+    fun start(client: AMQPClient) {
+
+        // Connect to Docker via Unix Sockets to create and manage containers
+        val config = DefaultDockerClientConfig.Builder().withDockerHost("unix:///var/run/docker.sock").build()
+        val httpClient = ApacheDockerHttpClient.Builder().dockerHost(config.dockerHost).build()
+        docker = DockerClientImpl.getInstance(config, httpClient)
+        logger.info("Connected to Docker.")
+
+        for((repo, defaultBranch) in repositoriesToVersionCheck) {
+            if (getMostRecentVersion(repo).isNullOrBlank()) {
+                logger.warn("No latest version information for $repo was found. Gathering latest version...")
+                fetchLatestVersion(repo, defaultBranch)
+            }
+        }
+
+        fixedRateTimer("Docker Container Monitoring", daemon = true, period = 30_000) {
+
+            val containers = docker.listContainersCmd().exec()
+
+            // Make sure the Puffin network exists and all containers are connected to it
+            val puffinNetworks = docker.listNetworksCmd().withNameFilter(PUFFIN_NETWORK_NAME).exec()
+            val puffinNetworkExists = puffinNetworks.isNotEmpty()
+            if (!puffinNetworkExists) {
+                // Create a new network
+                logger.warn("Puffin network does not exist, creating...")
+                val response = docker.createNetworkCmd().withName(PUFFIN_NETWORK_NAME).withDriver("bridge").exec()
+                puffinNetworkId = response.id
+                response.warnings?.forEach { logger.warn(it) }
+                logger.warn("Puffin network created with ID: ${response.id}")
+                // Connect all running containers to it
+                containers.forEach {
+                    docker.connectToNetworkCmd().withContainerId(it.id).withNetworkId(response.id).exec()
+                }
+                logger.warn("Connected ${containers.size} containers to the Puffin network.")
+            } else {
+                puffinNetworkId = puffinNetworks.first().id
+            }
+
+            containers.forEach { container ->
+                if(requiredNetworkAccess.contains(container.image)) {
+                    if(container.networkSettings?.networks?.containsKey(PUFFIN_NETWORK_NAME) == false) {
+                        logger.info("Connecting container ${container.image}/${container.id} to network $PUFFIN_NETWORK_NAME (id: $puffinNetworkId).")
+                        docker.connectToNetworkCmd().withContainerId(container.id).withNetworkId(puffinNetworkId).exec()
+                    }
+                }
+            }
+
+            // Make sure there are enough of each container type to satisfy the minimums from [numContainersByLabel]
+            for((requiredLabel, minimum, imageName) in numContainersByLabel) {
+                val serverContainers = containers.count {
+                    it.labels.containsKey(requiredLabel)
+                }
+                if (serverContainers < minimum) {
+                    logger.info("There are only $serverContainers containers with label $requiredLabel, but $minimum are required. Starting another.")
+                    startContainer(imageName(), UUID.randomUUID())
+                }
+            }
+        }
+    }
+
+    /**
+     * Get a [Container] from its [containerId]. This method can return null,
+     * which most likely means the container does not exist or is not running.
+     */
+    fun getRunningContainer(containerId: UUID): Container? {
+        val containers = docker.listContainersCmd().withNameFilter(listOf(containerId.toString())).exec()
+        return containers.firstOrNull()
+    }
+
+    /**
+     * Start a container using the most recent tag for [repository],
+     * giving the container an environment variable called `container_id`
+     * with the string contents of [containerId].
+     * The container is immediately connected to the Puffin network
+     * and is named after the [containerId].
+     * The container is started immediately after it is created.
+     */
+    private fun startContainer(repository: String, containerId: UUID) {
+        logger.info("Starting new container with containerId=$containerId")
+        val response = docker.createContainerCmd(getMostRecentTag(repository))
+            .withName(containerId.toString()) // container name
+            .withEnv("container_id", containerId.toString()) // pass containerId to the program in the container
+            .withHostConfig(HostConfig.newHostConfig()
+                .withNetworkMode(puffinNetworkId)) // put this container on the same network, so it can access the DB and messaging
+            .exec()
+        response.warnings?.forEach {
+            logger.warn(it)
+        }
+        logger.info("Container creation succeeded: container created with ID ${response.id}")
+        docker.startContainerCmd(response.id).exec()
+        logger.info("Container started successfully.")
+    }
+
+    /**
+     * Fetches the [branch] from the GitHub [repository], builds a Docker image from its latest commit,
+     * and assigns this new image as the server's latest version. This means that
+     * all new containers will use this version.
+     */
+    private fun fetchLatestVersion(repository: String, branch: String) {
+        val (tag, commit) = buildDockerImageFromRef(repository, branch)
+        if (tag != null && commit != null) {
+            setMostRecentVersion(repository, commit)
+            logger.info("Update completed.")
+        }
+    }
+
+    /**
+     * Fetches the [branch] from the GitHub [repository] and builds a Docker image from it.
+     * This method does not set the server's latest version.
+     * @return The tag assigned to the new Docker image paired with the latest commit sha of [branch]
+     */
+    private fun buildDockerImageFromRef(repository: String, branch: String): Pair<String?, String?> {
+        logger.info("Updating $repository to the latest version...")
+        val githubURL = getBlobURL(repository, branch)
+        val commit = getLatestCommitSha(repository, branch)
+        logger.info("Found blob URL from GitHub: $githubURL")
+        return buildDockerImage(repository, GzipCompressorInputStream(URL(githubURL).openStream()), commit) to commit
+    }
+
+    /**
+     * Get the latest commit SHA-1 from a GitHub [repository] on the specified [branch].
+     * Returns the full, un-truncated string.
+     */
+    private fun getLatestCommitSha(repository: String, branch: String): String {
+        val request = HttpRequest.newBuilder().GET()
+            .uri(URI.create("https://api.github.com/repos/$GITHUB_USER/$repository/commits/$branch"))
+            .header("Accept", "application/vnd.github.VERSION.sha").header("Authorization", "Token $githubToken")
+            .build()
+
+        val response = http.send(request, BodyHandlers.ofString())
+        if (response.statusCode() == 200) {
+            logger.info("Found latest commit SHA for branch $branch: ${response.body()}")
+            return response.body()
+        } else throw RuntimeException("Error received while querying GitHub API: ${response.statusCode()}; ${response.body()}")
+    }
+
+    /**
+     * Converts a TAR archive into another (gzipped) TAR archive with the first directory level removed.
+     */
+    private fun removeOuterFolder(input: InputStream, output: OutputStream) {
+        val outputStream = TarArchiveOutputStream(GzipCompressorOutputStream(BufferedOutputStream(output)))
+        val archive = TarFile(input.readAllBytes())
+
+        archive.entries.forEach { entry ->
+            entry.name = entry.name.substringAfter('/')
+            if (entry.name.isNotEmpty()) { // The root folder will be replaced by an empty string
+                outputStream.putArchiveEntry(entry)
+                IOUtils.copy(archive.getInputStream(entry), outputStream)
+                outputStream.closeArchiveEntry()
+            }
+        }
+        outputStream.close()
+    }
+
+    /**
+     * The GitHub personal access token used to contact the GitHub API and download code.
+     */
+    private val githubToken by lazy {
+        SavedProperties.getString("gh_token")
+    }
+
+    /**
+     * Get the URL which can be used to download a TAR archive containing the
+     * entire [repository].
+     */
+    private fun getBlobURL(repository: String, ref: String): String {
+        // Request an archive of the latest version of the repository and download it to a zip file
+        logger.info("Requesting archive link from GitHub...")
+        val req = HttpRequest.newBuilder().GET()
+            .uri(URI.create("https://api.github.com/repos/$GITHUB_USER/$repository/tarball/$ref"))
+            .header("Authorization", "Token $githubToken").build()
+        val res = http.send(req, HttpResponse.BodyHandlers.ofString())
+
+        val optional = res.headers().firstValue("location")
+        if (optional.isEmpty) logger.warn("No URL found")
+        return optional.get()
+    }
+
+    /**
+     * Build a Docker image using a TAR archive supplied by [urlInputStream].
+     * The image is tagged using the values of [repository] and [tag].
+     * By default, the image will receive two tags:
+     * - `bluedragonmc/{repository}:{version}`
+     * - `bluedragonmc/{repository}:latest`
+     *
+     * It will also be labelled with:
+     * `com.bluedragonmc.{repository}.version={tag}`
+     */
+    private fun buildDockerImage(repository: String, urlInputStream: InputStream, tag: String): String? {
+        logger.info("Building a new Docker image with tag: $tag")
+
+        val pb = ProcessBuilder("docker",
+            "build",
+            "-t", getTag(repository, tag),
+            "-t", getTag(repository, "latest"),
+            "--label", "${getVersionLabel(repository)}=$tag".lowercase(),
+            "-")
+        pb.environment()["DOCKER_BUILDKIT"] = "1"
+        pb.environment()["BUILDKIT_PROGRESS"] = "plain"
+        pb.redirectErrorStream(true)
+        val process = pb.start()
+
+        // Get the file at [blobURL] as a gzipped tar archive, remove the outer folder, and pipe the bytes to the Docker CLI.
+        removeOuterFolder(urlInputStream, process.outputStream)
+
+        BufferedReader(InputStreamReader(process.inputStream)).use { input ->
+            var line: String?
+            while (input.readLine().also { line = it } != null) {
+                logger.info(line)
+            }
+        }
+
+        val exitCode = process.waitFor()
+        return if (exitCode == 0) {
+            logger.info("Success! Docker image '$tag' has been built.")
+            getTag(repository, tag)
+        } else {
+            logger.error("Unexpected exit code from Docker CLI. See logs above for details.")
+            null
+        }
+    }
+
+    /**
+     * Forces a container's removal. The container will be killed if it is currently running, and then it will be removed.
+     */
+    fun removeContainer(container: Container) {
+        InstanceManager.onContainerRemoved(container.names.first().substringAfter('/'))
+        docker.removeContainerCmd(container.id).withForce(true).exec()
+    }
+
+    fun isLatestVersion(container: Container, repository: String): Boolean {
+        return container.labels[getVersionLabel(repository)] == getMostRecentVersion(repository)
+    }
+
+}
--- src/main/kotlin/com.bluedragonmc.puffin/InstanceManager.kt
+++ src/main/kotlin/com.bluedragonmc.puffin/InstanceManager.kt
@@ -3,6 +3,7 @@ package com.bluedragonmc.puffin
 import com.bluedragonmc.messages.GameType
 import com.bluedragonmc.messages.NotifyInstanceCreatedMessage
 import com.bluedragonmc.messages.NotifyInstanceRemovedMessage
+import com.bluedragonmc.messages.PingMessage
 import com.bluedragonmc.messagingsystem.AMQPClient
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -22,12 +23,18 @@ object InstanceManager {
     private val containers = mutableMapOf<UUID, MutableSet<UUID>>()
 
     fun start(client: AMQPClient) {
+        client.subscribe(PingMessage::class) { message ->
+            logger.info("New container started (received confirmation from Minestom server) - ${message.containerId}")
+            logger.info("Version info received: ${message.versionInfo}")
+            containers[message.containerId] = mutableSetOf()
+        }
         client.subscribe(NotifyInstanceCreatedMessage::class) { message ->
             // Add to map of instances to game types
             instanceTypes[message.instanceId] = message.gameType
 
             // Add to list of containers
             if (!containers.containsKey(message.containerId)) {
+                logger.warn("Instance was created without a PingMessage sent first. containerId=${message.containerId}, instanceId=${message.instanceId}")
                 containers[message.containerId] = mutableSetOf()
             }
             containers[message.containerId]!!.add(message.instanceId)
@@ -43,8 +50,19 @@ object InstanceManager {
             }
         }
         client.subscribe(NotifyInstanceRemovedMessage::class) { message ->
-            containers[message.containerId]!!.remove(message.instanceId)
+            containers[message.containerId]?.remove(message.instanceId)
             instanceTypes.remove(message.instanceId)
+
+            if(containers[message.containerId]!!.isEmpty()) {
+                // The removed instance was the container's last instance; it is currently not running any instances.
+                // Check if the container can be updated to a more recent version.
+                DockerContainerManager.getRunningContainer(message.containerId)?.let { container ->
+                    if(!DockerContainerManager.isLatestVersion(container, "Server")) {
+                        logger.info("Removing container ${container.names.first()} because it is running an outdated version.")
+                        DockerContainerManager.removeContainer(container)
+                    }
+                }
+            }
         }
     }
 
@@ -60,4 +78,10 @@ object InstanceManager {
     }
 
     fun findContainerWithLeastInstances() = containers.minByOrNull { (_, instances) -> instances.size }
+    fun onContainerRemoved(name: String) {
+        val uuid = UUID.fromString(name)
+        val localInstances = containers[uuid]
+        containers.remove(UUID.fromString(name))
+        localInstances?.forEach { instanceTypes.remove(it) }
+    }
 }
